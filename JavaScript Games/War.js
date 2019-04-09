71e413bfc10b12636dd7b359c0df48d9a491a182
Matthew Hefner <42611452+matthewhefner@users.noreply.github.com>
2019-04-08 21:30:55 -0400

    Update War.js

--- JavaScript Games/War.js
+++ JavaScript Games/War.js
@@ -1,4 +1,4 @@
-var active //2d boolean array of iteration
+var active; //2d boolean array of iteration
 var newActive; //2d boolean array of next iteration
 var alive; //2d boolean array of iteration
 var newAlive; //2d boolean array of next iteration
@@ -7,7 +7,7 @@ var cols; //int # of cols
 var paused; //boolean toggles resume/pause
 var neighbors; //2d int array of neighbors
 var rulers; //2d int array of rulers
-var scaler; //int size of each cell when drawn to screen
+var scalar; //int size of each cell when drawn to screen
 var blankArray; //for cleaning the arrays
 var water; //int array holding RGBA value of water
 
@@ -17,36 +17,36 @@ function preload() {
   playingMenuImg = loadImage('playingMenu.png');
   // TODO:
   //new colors
-  //world wrap
   //map options
-  //touch menu 
   //resources
 }
 
-function setup()
-{
+function setup() {
     var canvas = createCanvas(960,540);
     //setAttributes('antialias', true);
 	paused = true;
-	scaler = 1;
-	rows = 480 * 2;
-	cols = 270 * 2;
+	scalar = 1;
+	cols = width;
+	rows = height;
 	populateArrays();
 	background(0);
-	background(img, [0]);
+	image(img, 0, 0);
 	img.loadPixels();
-	water = [img.pixels[0], img.pixels[1], img.pixels[2], img.pixels[3]];
+	//TODO: Resources
+	//water = [img.pixels[0], img.pixels[1], img.pixels[2], img.pixels[3]];
+	//print(water);
 	frameRate(500);
 }
 
 function draw() {
 	if (!paused) {
 		generation();
-		background(img);
-		drawMenu();
-	} else {
-		drawMenu();
+		if (frameCount % 4 == 1) {
+			background('rgba(0%,0%,0%,0.01)');
+		}
+		image(img, 0, 0);
 	}
+	drawMenu();
 }
 
 function drawMenu() {
@@ -60,8 +60,6 @@ function drawMenu() {
 function keyPressed() {
 	if (key == ' ') {
 		pauseProcess();
-	} else if (key == 's') {
-		startMoves();
 	}
 	if (keyCode == DELETE) {
 		if (confirm("Are you sure you want to clear the map?")) {
@@ -74,14 +72,14 @@ function keyPressed() {
 function pauseProcess() {
 	paused = !paused;
 	if (paused) {
-		for (var k = 0; k < rows; k++) {
+		for (var k = 0; k < cols; k++) {
 			newActive[k] = blankArray[k].slice();
 			newAlive[k] = blankArray[k].slice();
 			newActive[k] = active[k].slice();
 			newAlive[k] = alive[k].slice();
 		}
 	} else {
-		for (var k = 0; k < rows; k++) {
+		for (var k = 0; k < cols; k++) {
 			active[k] = newActive[k].slice();
 			alive[k] = newAlive[k].slice();
 			newActive[k] = blankArray[k].slice();
@@ -98,63 +96,43 @@ function keyReleased() {
 function mouseDragged() {
 	if (paused)
 	{
-		var row = floor(mouseX / scaler);
-		var col = floor(mouseY / scaler);
+		var col = floor(mouseX / scalar);
+		var row = floor(mouseY / scalar);
 		switch(key) {
 			case '1':
+				rulers[col][row] = 1;
 				fill(77,175,74);
-				mouseHelper(row, col, 1);
-				mouseHelper(row + 1, col, 1);
-				mouseHelper(row, col + 1, 1);
-				mouseHelper(row + 1, col + 1, 1);
+				mouseHelper(col, row);
 				break;
 			case '2':
+				rulers[col][row] = 2;
 				fill(55,126,255);
-				mouseHelper(row, col, 2);
-				mouseHelper(row + 1, col, 2);
-				mouseHelper(row, col + 1, 2);
-				mouseHelper(row + 1, col + 1, 2);
+				mouseHelper(col, row);
 				break;
 			case '3':
+				rulers[col][row] = 3;
 				fill(255,26,28);
-				mouseHelper(row, col, 3);
-				mouseHelper(row + 1, col, 3);
-				mouseHelper(row, col + 1, 3);
-				mouseHelper(row + 1, col + 1, 3);
+				mouseHelper(col, row);
 				break;
 			case '4':
+				rulers[col][row] = 4;
 				fill(255,255,51);
-				mouseHelper(row, col, 4);
-				mouseHelper(row + 1, col, 4);
-				mouseHelper(row, col + 1, 4);
-				mouseHelper(row + 1, col + 1, 4);
+				mouseHelper(col, row);
 				break;
 		}
 	}
 }
 
-function startMoves() {
-	fill(55,126,255);
-	for (let u = 0; u < rows; u++) {
-		for (let v = 0; v < cols; v++) {
-			if ( (u) % 500 == 0 && u != 0) {
-				mouseHelper(u, v, 2);
-			}
-		}
-	}
-}
-
-function mouseHelper(row, col, rul) {
-	rulers[row][col] = rul;
-	newActive[row][col] = true;
-	newAlive[row][col] = true;
-	newLife(row, col);
+function mouseHelper(col, row) {
+	newActive[col][row] = true;
+	newAlive[col][row] = true;
+	newLife(col, row);
 	noStroke();
-	rect(row * scaler, col * scaler, scaler, scaler);
+	rect(col * scalar, row * scalar, scalar, scalar);
 }
 
 function mousePressed() {
-	let z = (rows * floor(mouseX / scaler) + floor(mouseY / scaler)) * 2 * 4;
+	let z = (rows * floor(mouseX / scalar) + floor(mouseY / scalar)) * 2 * 4;
 	print(img.width, img.height);
 	print(img.pixels[z]);
 	if ((mouseX < 224 || mouseY < 50)) {
@@ -170,32 +148,36 @@ function mousePressed() {
 			pauseProcess();
 		}
 	} else if (paused) {
-		var row = floor(mouseX / scaler);
-		var col = floor(mouseY / scaler);
+		var col = floor(mouseX / scalar);
+		var row = floor(mouseY / scalar);
 		switch(key) {
 			case '1':
+				rulers[col][row] = 1;
 				fill(77,175,74);
-				mouseHelper(row, col, 1);
+				mouseHelper(col, row);
 				break;
 			case '2':
+				rulers[col][row] = 2;
 				fill(55,126,255);
-				mouseHelper(row, col, 2);
+				mouseHelper(col, row);
 				break;
 			case '3':
+				rulers[col][row] = 3;
 				fill(255,26,28);
-				mouseHelper(row, col, 3);
+				mouseHelper(col, row);
 				break;
 			case '4':
+				rulers[col][row] = 4;
 				fill(255,255,51);
-				mouseHelper(row, col, 4);
+				mouseHelper(col, row);
 				break;
 		}
 	}
 }
 
 function generation() {
-	for (var i = 0; i < rows; i++) {
-		for (var j = 0; j < cols; j++) {
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
 			if (active[i][j]) {
 				if ((neighbors[i][j] == 2 || neighbors[i][j] == 3) & alive[i][j]) {
 					newActive[i][j] = true;
@@ -208,10 +190,7 @@ function generation() {
 					newAlive[i][j] = true;
 					drawRect(i, j);
 				} else if ((neighbors[i][j] > 6) & alive[i][j]) {
-					//Allows resource management.
-					//(rows * floor(mouseY / scaler) + floor(mouseX / scaler)) * 2 * 4;
-					let z = floor((rows * j * 2 + i) * 4 * 2);
-					if (img.pixels[z] == 0) {
+					if (onLand(i, j)) {
 						newActive[i][j] = true;
 						newAlive[i][j] = true;
 						drawRect(i, j);	
@@ -226,14 +205,18 @@ function generation() {
 			}
 		}
 	}
-	for (var k = 0; k < rows; k++) {
+	for (var k = 0; k < cols; k++) {
 		active[k] = newActive[k].slice();
 		alive[k] = newAlive[k].slice();
 		newActive[k] = blankArray[k].slice();
 		newAlive[k] = blankArray[k].slice();
 	}
-	
 	fullCheck();
+}
+
+function onLand(i, j) {
+	//There are 4 numbers in the array per pixel
+	return img.pixels[((width * j + i) * scalar * 4)] != 103;
 }
 
 function drawRect(i, j) {
@@ -253,7 +236,7 @@ function drawRect(i, j) {
 		default:
 			break;
 	} 
-	rect(i * scaler, j * scaler, scaler, scaler);
+	rect(i * scalar, j * scalar, scalar, scalar);
 }
 
 function newLife(i, j) {
@@ -261,49 +244,49 @@ function newLife(i, j) {
 	if (i > 0 && j > 0) {
 		newActive[i - 1][j - 1] = true;
 	} else if (j > 0){
-		newActive[rows - 1][j - 1] = true;
+		newActive[cols - 1][j - 1] = true;
 	}
 	//Left
 	if (i > 0) {
 		newActive[i - 1][j] = true;
 	} else {
-		newActive[rows - 1][j] = true;
+		newActive[cols - 1][j] = true;
 	}
 	//Bottom Left
-	if (i > 0 && j < cols - 1) {
+	if (i > 0 && j < rows - 1) {
 		newActive[i - 1][j + 1] = true;
-	} else if (j < cols - 1){
-		newActive[rows - 1][j + 1] = true;
+	} else if (j < rows - 1){
+		newActive[cols - 1][j + 1] = true;
 	}
 	//Top
 	if (j > 0) {
 		newActive[i][j - 1] = true;
 	}
 	//Bottom
-	if (j < cols - 1) {
+	if (j < rows - 1) {
 		newActive[i][j + 1] = true;
 	}
 	//Top Right
-	if (i < rows - 1 && j > 0) {
+	if (i < cols - 1 && j > 0) {
 		newActive[i + 1][j - 1] = true;
 	} else if (j > 0) {
 		newActive[0][j - 1] = true;
 	}
-	if (i < rows - 1) {
+	if (i < cols - 1) {
 		newActive[i + 1][j] = true;
 	} else {
 		newActive[0][j] = true;
 	}
-	if (i < rows - 1 && j < cols - 1) {
+	if (i < cols - 1 && j < rows - 1) {
 		newActive[i + 1][j + 1] = true;
-	} else if (j < cols - 1) {
+	} else if (j < rows - 1) {
 		newActive[0][j + 1] = true;
 	}
 }
 
 function fullCheck() {
-	for (var i = 0; i < rows; i++) {
-		for (var j = 0; j < cols; j++) {
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
 			if (active[i][j]) {
 				neighborCheck(i, j);
 			}
@@ -318,25 +301,25 @@ function neighborCheck(i, j) {
 	if (i > 0 && j > 0 && alive[i - 1][j - 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i - 1][j - 1]] += 1;
-	} else if (i == 0 && j > 0 && alive[rows - 1][j - 1]) {
+	} else if (i == 0 && j > 0 && alive[cols - 1][j - 1]) {
 		neighbors[i][j] += 1;
-		ruler[rulers[rows - 1][j - 1]] += 1;
+		ruler[rulers[cols - 1][j - 1]] += 1;
 	}
 	//left
 	if (i > 0 && alive[i - 1][j]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i - 1][j]] += 1;
-	} else if (i == 0 && alive[rows - 1][j]) {
+	} else if (i == 0 && alive[cols - 1][j]) {
 		neighbors[i][j] += 1;
-		ruler[rulers[rows - 1][j]] += 1;
+		ruler[rulers[cols - 1][j]] += 1;
 	}
 	//bottom left
-	if (i > 0 && j < cols - 1 && alive[i - 1][j + 1]) {
+	if (i > 0 && j < rows - 1 && alive[i - 1][j + 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i - 1][j + 1]] += 1;
-	} else if (i == 0 && j < cols - 1 && alive[rows - 1][j + 1]) {
+	} else if (i == 0 && j < rows - 1 && alive[cols - 1][j + 1]) {
 		neighbors[i][j] += 1;
-		ruler[rulers[rows - 1][j + 1]] += 1;
+		ruler[rulers[cols - 1][j + 1]] += 1;
 	}
 	
 	//top
@@ -345,32 +328,32 @@ function neighborCheck(i, j) {
 		ruler[rulers[i][j - 1]] += 1;
 	}
 	//bottom
-	if (j < cols - 1 && alive[i][j + 1]) {
+	if (j < rows - 1 && alive[i][j + 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i][j + 1]] += 1;
 	}
 	
 	//top right
-	if (i < rows - 1 && j > 0 && alive[i + 1][j - 1]) {
+	if (i < cols - 1 && j > 0 && alive[i + 1][j - 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i + 1][j - 1]] += 1;
-	} else if (i == rows - 1 && j > 0 && alive[0][j - 1]) {
+	} else if (i == cols - 1 && j > 0 && alive[0][j - 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[0][j - 1]] += 1;
 	}
 	//right
-	if (i < rows - 1 && alive[i + 1][j]) {
+	if (i < cols - 1 && alive[i + 1][j]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i + 1][j]] += 1;
-	} else if (i == rows - 1 && alive[0][j]) {
+	} else if (i == cols - 1 && alive[0][j]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[0][j]] += 1;
 	}
 	//bottom right
-	if (i < rows - 1 && j < cols - 1 && alive[i + 1][j + 1]) {
+	if (i < cols - 1 && j < rows - 1 && alive[i + 1][j + 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[i + 1][j + 1]] += 1;
-	} else if (i == rows - 1 && j < cols - 1 && alive[0][j + 1]) {
+	} else if (i == cols - 1 && j < rows - 1 && alive[0][j + 1]) {
 		neighbors[i][j] += 1;
 		ruler[rulers[0][j + 1]] += 1;
 	}
@@ -402,7 +385,7 @@ function populateArrays() {
 	neighbors = [];
 	rulers = [];
 	blankArray = [];
-	for (var i = 0; i < rows; i++) {
+	for (var i = 0; i < cols; i++) {
 		active.push([]);
 		newActive.push([]);
 		alive.push([]);
@@ -410,7 +393,7 @@ function populateArrays() {
 		neighbors[i] = [];
 		rulers[i] = [];
 		blankArray.push([]);
-		for (var j = 0; j < cols; j++) {
+		for (var j = 0; j < rows; j++) {
 			active[i].push(false);
 			newActive[i].push(false);
 			alive[i].push(false);
